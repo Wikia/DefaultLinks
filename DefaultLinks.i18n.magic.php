41727e627e08e3a95ee62e5999942fdfdfc6d267
t-tomalak <ttomalak@fandom.com>
2022-03-30 17:14:09 +0200

    CATS-2348 | Update DefaultLinks extension to work with MW 1.37
    Switched onArticleDeleteComplete hook to PageDeleteComplete, and removed usage of MagiWord::get method to MagicWordFactory.

--- DefaultLinks.i18n.magic.php
+++ DefaultLinks.i18n.magic.php
@@ -3,16 +3,15 @@
  * DefaultLinks
  * DefaultLinks Magic Words
  *
- * @package		DefaultLinks
- * @link		https://help.gamepedia.com/Extension:DefaultLinks
+ * @package        DefaultLinks
+ * @link        https://help.gamepedia.com/Extension:DefaultLinks
  *
- **/
+ */
 
 $magicWords = [];
 
-$magicWords['en']  = [
-	'silent'		=> [0, 'silent'],
-	'defaultlink'	=> [0, 'DEFAULTLINK'],
-	'nodefaultlink'	=> [1, '__NODEFAULTLINKS__']
+$magicWords['en'] = [
+	'silent' => [ 0, 'silent' ],
+	'defaultlink' => [ 0, 'DEFAULTLINK' ],
+	'nodefaultlink' => [ 1, '__NODEFAULTLINKS__' ],
 ];
-?>
